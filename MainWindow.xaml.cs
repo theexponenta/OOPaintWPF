90824d5beba876a6efaa6d4be82f292d1d9d4dda
Exponenta <the.exponenta@gmail.com>
2025-03-16 02:49:11 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPaint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
